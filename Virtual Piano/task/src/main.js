ec720e14cb1a6ed72bd21df4a69113fa9a4165b4
Ben Buske <info@buske-it.com>
2021-04-20 10:52:24 -0400

    another commit

---
+++ Virtual Piano/task/src/main.js
@@ -0,0 +1,45 @@
+document.addEventListener("keydown", function(event) {
+        if (event.key === "a") {
+            let a = new Audio("snd/A.mp3");
+            a.play();
+        }   else if (event.key === 's') {
+            let s = new Audio("snd/S.mp3");
+            s.play();
+        }   else if (event.key === "d") {
+            let d = new Audio("snd/D.mp3");
+            d.play();
+        }   else if (event.key === "f") {
+            let f = new Audio("snd/F.mp3");
+            f.play();
+        }   else if (event.key === "g") {
+            let g = new Audio("snd/G.mp3");
+            g.play();
+        }   else if (event.key === "h") {
+            let h = new Audio("snd/H.mp3");
+            h.play();
+        }   else if (event.key === "j") {
+            let j = new Audio("snd/J.mp3");
+            j.play();
+        }   else if (event.key === "w") {
+            let j = new Audio("snd/W.mp3");
+            j.play();
+        }   else if (event.key === "e") {
+            let j = new Audio("snd/E.mp3");
+            j.play();
+        }   else if (event.key === "t") {
+            let t = new Audio("snd/T.mp3");
+            t.play();
+        }   else if (event.key === "y") {
+            let y = new Audio("snd/Y.mp3");
+            y.play();
+        }   else if (event.key === "u") {
+            let u = new Audio("snd/U.mp3")
+            u.play();
+        }   else {
+            console.log("Key not recognized.");
+        }
+});
+
+
+
+
